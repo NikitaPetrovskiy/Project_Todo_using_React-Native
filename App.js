37f3ef43f5dc1de2b5b7e23527a1f671c8ccc167
Nikita Petrovskiy <n.petrovsky@richbrains.net>
2019-12-13 18:34:06 +0300

    Created basic navbar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavBar } from './src/NavBar';
+
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <NavBar title="Todo App"/>
     </View>
   );
 }
